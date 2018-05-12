fbe597a99d7310ee1098feabe13a2ccddfd940f2
Pedro Carvalho <pedrozwpiper@gmail.com>
2018-05-12 16:56:09 -0300

    Validação formulário Projetos OK

--- app/Http/Controllers/ProjetosController.php
+++ app/Http/Controllers/ProjetosController.php
@@ -6,6 +6,8 @@ use App\Cliente;
 use App\Projeto;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
+
 class ProjetosController extends Controller
 {
    public function projetos()
@@ -31,6 +33,13 @@ class ProjetosController extends Controller
 
    public function CriaProjeto(Request $request)
    {
+      $validacao = $this->validacao($request->all());
+      if($validacao->fails()){
+         return redirect()->back()
+            ->withErrors($validacao->errors())
+            ->withIputs($request->all());
+      }
+
    	Projeto::create($request->all());
    	return redirect('/home/projetos')->with('message', "Projeto criado com sucesso!");
    }
@@ -42,5 +51,23 @@ class ProjetosController extends Controller
       } catch (\Exception $e) {
          return "ERRO: " . $e->getMessage();
       }
+   }
+
+   private function validacao($data)
+   {
+      $regras['tipo'] = 'required';
+      $regras['nome'] = 'required|min:4|max:100';
+      $regras['prazoEntrega'] = 'required';
+      $regras['valor'] = 'required';
+
+      $mensagens = [
+         'tipo.required' => 'Campo Tipo Obrigatório',
+         'nome.required' => 'Campo Nome obrigatório',
+         'nome.min' => 'Nome deve ter no mínimo 4 digitos',
+         'nome.max' => 'Nome deve ter no máximo 4 digitos',
+         'prazoEntrega.required' =>  'Campo Prazo de Entrega Obrigatório',
+         'valor.required' => 'Campo Valor Obrigatório / Use 0 caso seja grátis'
+      ];
+      return Validator::make($data, $regras, $mensagens);  
    }
 }
--- resources/views/projetos/adicionarProjeto.blade.php
+++ resources/views/projetos/adicionarProjeto.blade.php
@@ -7,41 +7,86 @@
 	<div class="col-md-6 well">
 		<form action="{{ url('home/criarProjeto') }}" method="POST">
 			{{ csrf_field() }}
+			<div class="form-group col-md-12 {{ $errors->has('tipo') ? 'has-error' : ''}}">
+				<label for="tipo" class="control-label"><h6>Tipo de Projeto:</h6></label>
+				<input type="text" name="tipo" id="tipo" class="form-control" placeholder="Site, Software, Consultoria, Design Gráfico">
+				@if($errors->has('tipo'))
+					{{ $errors->first('tipo') }}
+				@endif
+			</div>
+			<div class="form-group col-md-12 {{ $errors->has('nome') ? 'has-error' : ''}}">
+				<label for="nome"><h6>Nome do Projeto:</h6></label>
+				<input type="text" name="nome" id="nome" class="form-control" placeholder="Nome do Projeto" value="{{ old('nome') }}">
+				@if($errors->has('nome'))
+					{{ $errors->first('nome') }}
+				@endif
+			</div>
+			<div class="form-group col-md-12 {{ $errors->has('prazoEntrega') ? 'has-error' : ''}}">
+				<label for="prazoEntrega"><h6>Prazo de Entrega:</h6></label>
+				<input type="number" name="prazoEntrega" id="prazoEntrega" class="form-control" placeholder="1" max="42">
+				@if($errors->has('prazoEntrega'))
+					{{ $errors->first('prazoEntrega') }}
+				@endif
+			</div>
 			<div class="form-group col-md-12">
-				<label for="tipo" class="control-label">Tipo de Projeto:</label>
-				<input type="text" name="tipo" id="tipo" class="form-control" placeholder="Site, Software, Consultoria, Design Gráfico" Required>
-				<label for="nome">Nome do Projeto:</label>
-				<input type="text" name="nome" id="nome" class="form-control" placeholder="Nome do Projeto" Required>
-				<label for="prazoEntrega">Prazo de Entrega:</label>
-				<input type="number" name="prazoEntrega" id="prazoEntrega" class="form-control" placeholder="1" max="42" Required>
-				<input type="radio" name="tipoPrazoEntrega" id="tipoPrazoEntrega1" value="dias" Required>
-				<label for="tipoPrazoEntrega1">Dias</label>
-				<input type="radio" name="tipoPrazoEntrega" id="tipoPrazoEntrega2" value="Meses" Required>
-				<label for="tipoPrazoEntrega2">Meses</label>
-				<input type="radio" name="tipoPrazoEntrega" id="tipoPrazoEntrega3" value="Anos" Required>
-				<label for="tipoPrazoEntrega3">Anos</label>
-				<input type="radio" name="tipoPrazoEntrega" id="tipoPrazoEntrega4" value="Contrato" Required>
-				<label for="tipoPrazoEntrega4">Contrato</label><br/>
-				<label for="valor">Valor R$:</label>
+				<p><label for="tipoPrazoEntrega1">
+					<input type="radio" name="tipoPrazoEntrega" id="tipoPrazoEntrega1" value="dias" Required>
+					<span>Dias</span>
+				</label></p>
+				<p><label for="tipoPrazoEntrega2">
+					<input type="radio" name="tipoPrazoEntrega" id="tipoPrazoEntrega2" value="Meses" Required>
+					<span>Meses</span>
+				</label></p>
+				<p><label for="tipoPrazoEntrega3">
+					<input type="radio" name="tipoPrazoEntrega" id="tipoPrazoEntrega3" value="Anos" Required>
+					<span>Anos</span>
+				</label></p>
+				<p><label for="tipoPrazoEntrega4">
+					<input type="radio" name="tipoPrazoEntrega" id="tipoPrazoEntrega4" value="Contrato" Required>
+					<span>Contrato</span>
+				</label></p><br/>
+			</div>
+			<div class="form-group col-md-12 {{ $errors->has('valor') ? 'has-error' : ''}}">
+				<label for="valor"><h6>Valor R$:</h6></label>
 				<input type="number" name="valor" id="valor" class="form-control" placeholder="1000" Required>
-				<label for="metodoPagamento">Método de Pagamento:</label>
-				<input type="radio" name="metodoPagamento" id="metodoPagamento1" value="Crédito" Required>
-				<label for="metodoPagamento1">Crédito</label>
-				<input type="radio" name="metodoPagamento" id="metodoPagamento2" value="Dinheiro" Required>
-				<label for="metodoPagamento2">Dinheiro</label>
-				<input type="radio" name="metodoPagamento" id="metodoPagamento3" value="Cheque" Required>
-				<label for="metodoPagamento3">Cheque</label>
-				<label for="parcelasPagamento">Parcelas de Pagamento:</label>
-				<input type="number" name="parcelasPagamento" id="parcelasPagamento" class="form-control" placeholder="(1 Caso seja à vista)" max="42" Required>
-				<input type="radio" name="tipoParcelasPagamento" id="tipoParcelasPagamento1" value="dias" Required>
-				<label for="tipoParcelasPagamento1">Dias</label>
-				<input type="radio" name="tipoParcelasPagamento" id="tipoParcelasPagamento2" value="Meses" Required>
-				<label for="tipoParcelasPagamento2">Meses</label>
-				<input type="radio" name="tipoParcelasPagamento" id="tipoParcelasPagamento3" value="Anos" Required>
-				<label for="tipoParcelasPagamento3">Anos</label>
-				<input type="radio" name="tipoParcelasPagamento" id="tipoParcelasPagamento4" value="aVista" Required>
-				<label for="tipoParcelasPagamento4">À Vista</label><br/>
-				<label for="cliente_id">Id do Cliente:</label>
+				@if($errors->has('valor'))
+					{{ $errrors->first('valor') }}
+				@endif
+			</div>	
+				<label for="metodoPagamento"><h6>Método de Pagamento:</h6></label>
+				<p><label for="metodoPagamento1">
+					<input type="radio" name="metodoPagamento" id="metodoPagamento1" value="Crédito" Required>
+					<span>Crédito</span>
+				</label></p>
+				<p><label for="metodoPagamento2">
+					<input type="radio" name="metodoPagamento" id="metodoPagamento2" value="Dinheiro" Required>
+					<span>Dinheiro</span>
+				</label></p>
+				<p><label for="metodoPagamento3">
+					<input type="radio" name="metodoPagamento" id="metodoPagamento3" value="Cheque" Required>
+					<span>Cheque</span>
+				</label></p>
+				<div class="form-group col-md-12">
+					<label for="parcelasPagamento"><h6>Parcelas de Pagamento:</h6></label>
+					<input type="number" name="parcelasPagamento" id="parcelasPagamento" class="form-control" placeholder="(1 Caso seja à vista)" max="42" Required>
+				</div>
+				<p><label for="tipoParcelasPagamento1">
+					<input type="radio" name="tipoParcelasPagamento" id="tipoParcelasPagamento1" value="dias" Required>
+					<span>Dias</span>
+				</label></p>
+				<p><label for="tipoParcelasPagamento2">
+					<input type="radio" name="tipoParcelasPagamento" id="tipoParcelasPagamento2" value="Meses" Required>
+					<span>Meses</span>
+				</label></p>
+				<p><label for="tipoParcelasPagamento3">
+					<input type="radio" name="tipoParcelasPagamento" id="tipoParcelasPagamento3" value="Anos" Required>
+					<span>Anos</span>
+				</label></p>
+				<p><label for="tipoParcelasPagamento4">
+					<input type="radio" name="tipoParcelasPagamento" id="tipoParcelasPagamento4" value="aVista" Required>
+					<span>À Vista</span>
+				</label></p><br/>
+				<label for="cliente_id"><h6>Id do Cliente:</h6></label>
 				<input type="number" name="cliente_id" id="cliente_id" class="form-control" Required>
 			</div>
 				<button type="submit" class="btn btn-primary">Adicionar Projeto</button>
